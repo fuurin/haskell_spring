d5bb46e5969f23a2567b541cf64bdd1b786a7405
fuurin <komatsu0624@docomo.ne/jp>
2015-02-26 05:23:37 +0900

    conanO'Brain 追加

--- Baby.hs
+++ Baby.hs
@@ -7,6 +7,11 @@ module Baby where
 --
 -- >>> doubleMe 8.3
 -- 16.6
+doubleMe x = x + x
+
+
+
+-- | doubleUs doubles two integer number and plus each 
 --
 -- >>> doubleUs 4 9
 -- 26
@@ -16,25 +21,39 @@ module Baby where
 --
 -- >>> doubleUs 28 88 + doubleMe 123
 -- 478
--- 
+doubleUs x y = x * 2 + y * 2
+
+
+
+-- | doubleSmallNumber doubles an integer number
+-- | when it is smaller than 101
+--
 -- >>> doubleSmallNumber 100
 -- 200
 --
 -- >>> doubleSmallNumber 101
 -- 101
 --
+doubleSmallNumber x = 	if x > 100
+						then x
+						else x*2
+
+
+
+-- | doubleSmallNumber' doubles an integer number 
+-- | when it is smaller than 101 and add 1
+--
 -- >>> doubleSmallNumber' 100
 -- 201
 --
 -- >>> doubleSmallNumber' 101
 -- 102
-
-doubleMe x = x + x
-
-doubleUs x y = x * 2 + y * 2
-
-doubleSmallNumber x = 	if x > 100
-						then x
-						else x*2
-
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+
+
+-- | conanO'Brain display specific string
+-- 
+-- >>> conanO'Brain
+-- "It's a-me, Conan O'Brain!"
+conanO'Brain = "It's a-me, Conan O'Brain!"
