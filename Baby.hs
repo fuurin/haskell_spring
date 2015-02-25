6d3a0b9fbdb2dafa502c9af251bf01296f2c30ac
fuurin <komatsu0624@docomo.ne/jp>
2015-02-26 00:49:29 +0900

    doubleUsを追加

--- Baby.hs
+++ Baby.hs
@@ -7,5 +7,16 @@ module Baby where
 --
 -- >>> doubleMe 8.3
 -- 16.6
+--
+-- >>> doubleUs 4 9
+-- 26
+--
+-- >>> doubleUs 2.3 34.2
+-- 73.0
+--
+-- >>> doubleUs 28 88 + doubleMe 123
+-- 478
 
 doubleMe x = x + x
+
+doubleUs x y = x * 2 + y * 2
