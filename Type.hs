ec6e949b308744f9b3c191ca13530ee200ead209
fuurin <komatsu0624@docomo.ne/jp>
2015-02-28 02:36:17 +0900

    2章完了

--- Type.hs
+++ Type.hs
@@ -13,3 +13,29 @@ removeNonUppercase st = [ c | c <- st, c `elem` ['A' .. 'Z'] ]
 -- 6
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
+
+
+
+-- | factorial makes factorial 1 to "n"
+-- >>> factorial 50
+-- 30414093201713378043612608166064768844377641568960512000000000000
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+
+
+-- | circumference makes a length of circumference of a circle
+-- | which have "r" meters of radius in "Float"
+-- >>> circumference 4.0
+-- 25.132742
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+
+
+-- | circumference' makes a length of circumference of a circle
+-- | which have "r" meters of radius in "Douoble"
+-- >>> circumference' 4.0
+-- 25.132741228718345
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
